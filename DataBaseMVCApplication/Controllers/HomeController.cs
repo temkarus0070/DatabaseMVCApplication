4d0d9fda5a21dac31ea1d9f52dca023c3ab9183c
temkarus0070 <pretkovartyom@gmail.com>
2020-11-13 10:14:51 +0300

    Add ManufactorFilter to Windows

--- DataBaseMVCApplication/Controllers/HomeController.cs
+++ DataBaseMVCApplication/Controllers/HomeController.cs
@@ -10,28 +10,55 @@ namespace DataBaseMVCApplication.Controllers
 {
     public class HomeController : Controller
     {
-        private WindowsServices windowsServices = new WindowsServices();
-        public ActionResult Index()
+        private Services.Services services = new Services.Services();
+        public ActionResult Index(int? manufactorId)
         {
-            var windows = windowsServices.GetWindows();
-            var windowsViewModelList = new List<WindowViewModel>();
-            windowsViewModelList = windows.Select(window => new WindowViewModel()
+            var manufactors = services.manufactorService.GetManufactors().Select(e => new ManufactorViewModel()
             {
-                Color = window.Color,
-                Description = window.Description,
-                Having = window.Having,
-                Id = window.Id,
-                Image = window.Image,
-                ManufactorId = window.ManufactorId,
-                ManufactorName = window.Manufactor.Name,
-                Price = window.Price
+                Address = e.Address,
+                Email = e.Email,
+                Id = e.Id,
+                Name = e.Name,
+                Phone = e.Phone
             }).ToList();
-            return View(windowsViewModelList);
+
+            manufactors.Insert(0, new ManufactorViewModel() { Id = 0, Name = "Все" });
+            var windows = services.windowsServices.GetWindows();
+            var windowsViewModelList = new List<WindowViewModel>();
+            if (manufactorId == null || manufactorId == 0)
+            {
+                windowsViewModelList = windows.Select(window => new WindowViewModel()
+                {
+                    Color = window.Color,
+                    Description = window.Description,
+                    Having = window.Having,
+                    Id = window.Id,
+                    Image = window.Image,
+                    ManufactorId = window.ManufactorId,
+                    ManufactorName = window.Manufactor.Name,
+                    Price = window.Price
+                }).ToList();
+            }
+            else
+            {
+                windowsViewModelList = windows.Where(e => e.ManufactorId == manufactorId).Select(window => new WindowViewModel()
+                {
+                    Color = window.Color,
+                    Description = window.Description,
+                    Having = window.Having,
+                    Id = window.Id,
+                    Image = window.Image,
+                    ManufactorId = window.ManufactorId,
+                    ManufactorName = window.Manufactor.Name,
+                    Price = window.Price
+                }).ToList();
+            }
+            return View((windowsViewModelList, manufactors));
         }
 
         public ActionResult Show(long index)
         {
-            var window = windowsServices.GetWindow(index);
+            var window = services.windowsServices.GetWindow(index);
             var windowVM = new WindowViewModel()
             {
                 Color = window.Color,
